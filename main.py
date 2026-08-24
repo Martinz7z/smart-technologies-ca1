7aa6086633970bd90adb9cc7170cf5f843a48681
Martin Zachariasz <Martin.zachariasz@gmail.com>
2026-08-24 18:21:30 +0100

    Set up TensorFlow project environment

---
+++ main.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import numpy as np
+
+
+def main():
+    print("Smart Technologies CA1")
+    print("TensorFlow version:", tf.__version__)
+    print("NumPy version:", np.__version__)
+
+
+if __name__ == "__main__":
+    main()
